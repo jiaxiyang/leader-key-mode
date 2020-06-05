2578ec46b86838addc3bc829df9a62cd32d162d1
jiaxiyang <jiaxiyangbit@126.com>
2020-06-05 13:10:41 +0800

    Update leader-key-mode.el

--- leader-key-mode.el
+++ leader-key-mode.el
@@ -91,6 +91,7 @@
     (define-key m (leader-key-mode-kbd "d d") 'kill-whole-line)
     (define-key m (leader-key-mode-kbd "d l") 'leader-key-mode--duplicate-line)
     (define-key m (leader-key-mode-kbd "w w") 'delete-window)
+    (define-key m (leader-key-mode-kbd "t w") 'toggle-windows-split)
     (define-key m (leader-key-mode-kbd "n") 'next-line)
     ;;(define-key m (leader-key-mode-kbd "u") 'undo)
     ;;(define-key m (leader-key-mode-kbd ":") pp-eval-expression)
@@ -249,6 +250,27 @@ This is common convention for many editors.  B is the beginnin of
         (t "xsel -ob"))
        1))
    ))
+
+;; Toggle between split windows and a single window
+(defun toggle-windows-split()
+  "Switch back and forth between one window and whatever split of windows we might have in the frame. The idea is to maximize the current buffer, while being able to go back to the previous split of windows in the frame simply by calling this command again."
+  (interactive)
+  (if (not (window-minibuffer-p (selected-window)))
+      (progn
+        (if (< 1 (count-windows))
+            (progn
+              (window-configuration-to-register ?u)
+              (delete-other-windows))
+          (jump-to-register ?u))))
+  (my-iswitchb-close))
+
+;; Note: you may also need to define the my-iswitchb-close function 
+;; created by Ignacio as well: http://emacswiki.org/emacs/IgnacioPazPosse
+(defun my-iswitchb-close()
+ "Open iswitchb or, if in minibuffer go to next match. Handy way to cycle through the ring."
+ (interactive)
+ (if (window-minibuffer-p (selected-window))
+    (keyboard-escape-quit)))
 
 (provide 'leader-key-mode)
 ;;; leader-key-mode.el ends here
